63618c88731591c49669db57f5a8bede9f0fd1c2
Dario Tarantini <dariotarantinimail@gmail.com>
2019-07-29 22:15:45 +0200

    e

--- install.sh
+++ install.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-if [ "$EUID" -ne 0 ]
-  then 
-    echo "Please run this script as root"
-    exit
-fi
 wget https://raw.githubusercontent.com/vpervenditti/vbf/master/src/vbf.v
 v -prod vbf.v
 mv vbf /usr/local/bin/vbf
