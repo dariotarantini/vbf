fbc6b05b466d007b5c920ad1163fb786b7018a85
Dario Tarantini <dariotarantinimail@gmail.com>
2019-07-30 14:47:38 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+if [ "$EUID" -ne 0 ]
+  then 
+    echo "Please run this script as root"
+    exit
+fi 
 git clone https://github.com/vpervenditti/vbf
-v -prod vbf/src/
-mv vbf/src/vbf /usr/local/bin/vbf
+rm -rf .a.out
+rm -rf /usr/local/bin/vbf
+v -prod ./vbf/vbf/.
+mv a.out /usr/local/bin/vbf
 rm -rf vbf
 echo Done!
 exit
