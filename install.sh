a8c5abda3a13653278cf49b2c4046255a62a788f
Dario Tarantini <dariotarantinimail@gmail.com>
2019-07-31 18:18:51 +0200

    fix installer

--- install.sh
+++ install.sh
@@ -3,12 +3,12 @@ if [ "$EUID" -ne 0 ]
   then 
     echo "Please run this script as root"
     exit
-fi 
-git clone https://github.com/vpervenditti/vbf
-rm -rf .a.out
+fi
+rm -rf vbf.v
 rm -rf /usr/local/bin/vbf
-v -prod ./vbf/vbf/.
-mv .a.out /usr/local/bin/vbf
+wget https://raw.githubusercontent.com/vpervenditti/vbf/master/vbf/vbf.v
+v -prod vbf.v
+mv vbf /usr/local/bin/vbf
 rm -rf vbf
 echo Done!
 exit
